5d2c64e3c0ee71ead1b8e11138961da5b052e94d
JSlush611 <jschlues611@gmail.com>
2025-02-12 15:32:03 -0600

    renaming / paths

--- p2p-frontend/src/components/StaticGraphs.jsx
+++ p2p-frontend/src/components/StaticGraphs.jsx
@@ -8,61 +8,61 @@ function StaticGraphs() {
       <Grid container spacing={4}>
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/average-time-by-year'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/average-time-by-year'}
             title="Average Time"
           />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/median-time-by-year'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/median-time-by-year'}
             title="Median Time"
           />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/participation-by-year'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/participation-by-year'}
             title="Overall Participation"
           />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/average-time-by-age-group-and-year'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/average-time-by-age-group-and-year'}
             title="Average Time by Age Group"
           />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/participation-by-region'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/participation-by-region'}
             title="Regional Participation"
           />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/time-percentiles-by-year'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/time-percentiles-by-year'}
             title="Time Percentiles"
           />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/gender-distribution-by-year'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/gender-distribution-by-year'}
             title="Gender Distribution"
           />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/top-10-fastest-swimmers'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/top-10-fastest-swimmers'}
             title="Top 10 Fastest Swimmers"
           />
         </Grid>
         {/* <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'process.env.REACT_APP_API_URI' + '/api/age-vs-time-distribution'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/age-vs-time-distribution'}
             title="Age vs Time"
           />
         </Grid> */}
         <Grid item xs={12} sm={6} md={4}>
           <GraphContainer
-            apiUrl={'http://127.0.0.1:5000' + '/api/participation-by-age-group'}
+            apiUrl={process.env.REACT_APP_API_URI + '/api/participation-by-age-group'}
             title="Age Group Participation"
           />
         </Grid>
